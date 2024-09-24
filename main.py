45c4e7f61ae49f99d9a6b426de4bb637ccfcabba
Asma Al Qadi <asmaalqadi@10-17-73-143.dynapool.wireless.nyu.edu>
2024-09-24 14:45:05 -0400

    Small Changes

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 
-# API Key from .env
+# API Key from .env 
 TICKETMASTER_API_KEY = os.getenv('TICKETMASTER_API_KEY')
 BASE_URL_DO_NYC = "https://donyc.com"
 
+# Searchies for events via Ticketmaster API on a given date
 def search_events_from_ticketmaster(date):
-    """Search for events via Ticketmaster API on a given date"""
-    # Adjust for potential timezone offset by starting from midnight of the given day
+    # Adjusted for potential timezone offset by starting from midnight of the given day
     start_date_str = datetime.combine(date, datetime.min.time()) + timedelta(hours=12)  # noon of the day to avoid time zone issues
     end_date_str = start_date_str + timedelta(days=1, seconds=-1)  # just before midnight of the same day
     start_date_iso = start_date_str.isoformat() + 'Z'
@@ -24,13 +24,13 @@ def search_events_from_ticketmaster(date):
     response = requests.get(url)
     if response.status_code == 200 and '_embedded' in response.json():
         events = response.json()['_embedded']['events']
-        return [{'name': event['name'], 'url': event['url']} for event in events]
+        return [{'Event Name': event['name'], 'Link for Event': event['url']} for event in events]
     else:
         print(f"Error searching events from Ticketmaster: {response.status_code}, Response: {response.text}")
         return []
-
+    
+# Scrapes the 'Do NYC' website for events on a given date
 def scrape_do_nyc(date):
-    """Scrapes the 'Do NYC' website for events on a given date"""
     date_str = date.strftime('%Y/%m/%d')  # Adjust to match 'Do NYC' date format in URL
     url = f"{BASE_URL_DO_NYC}/events/{date_str}"
     response = requests.get(url)
@@ -52,15 +52,15 @@ def main():
     date_input = input()
     selected_date = datetime.strptime(date_input, '%Y-%m-%d')
     
-    # Fetch events from Ticketmaster
+    # Gets the  events from Ticketmaster APi
     tm_events = search_events_from_ticketmaster(selected_date)
     tm_events_df = pd.DataFrame(tm_events)
 
-    # Scrape events from 'Do NYC'
+    # Scrapes the  events from 'Do NYC'
     donyc_events = scrape_do_nyc(selected_date)
     donyc_events_df = pd.DataFrame(donyc_events)
 
-    # Combine both dataframes
+    # Combines both dataframes into one CSV file of all events 
     combined_df = pd.concat([tm_events_df, donyc_events_df], ignore_index=True, sort=False)
     if not combined_df.empty:
         combined_df.to_csv('combined_events.csv', index=False)
